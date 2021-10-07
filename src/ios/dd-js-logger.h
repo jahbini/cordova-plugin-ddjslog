ccaefd4b9d570b77c09715c37c8875be82ce2a3d
Jim Hinds <jahbini@icloud.com>
2021-10-07 16:44:02 +0700

    Update dd-js-logger.h
    Xcode needs new definition

--- src/ios/dd-js-logger.h
+++ src/ios/dd-js-logger.h
@@ -1,5 +1,9 @@
 #import <Cordova/CDV.h>
-
+#ifdef DEBUG
+#define DDLogInfo(...) NSLog(@"%s(%p) %@", __PRETTY_FUNCTION__, self, [NSString stringWithFormat:__VA_ARGS__])
+#else
+#define DDLogInfo(...)
+#endif
 @interface DDJSLogger : CDVPlugin
 
 - (void) logio:(CDVInvokedUrlCommand*)command;
